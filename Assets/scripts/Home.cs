223898503b9c7735ee44aecf49b30b7f89780cac
fatdai <1012607376@qq.com>
2015-07-16 18:42:43 +0800

    init
    init

---
+++ Assets/scripts/Home.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Home : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+		mainCamera = Camera.main;
+
+		Debug.Log (Screen.width+":"+Screen.height);
+		Screen.SetResolution (Screen.width, Screen.height, true);
+//		float aspect = Screen.width / Screen.height;
+//		mainCamera.aspect = aspect;
+//		mainCamera.orthographicSize = Screen.height / 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/Resolution.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resolution : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		mainCamera.aspect = 1136f / 640f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
